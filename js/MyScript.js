732102211efb8b5ba7df9c93f553e2376d0bf03a
Donkó Márk <donkomark@gmail.com>
2020-10-06 22:37:16 +0200

    Probélkozás a data beolvasására

--- js/MyScript.js
+++ js/MyScript.js
@@ -1,16 +1,24 @@
 function readFile(){
     jQuery.get('assets/data.txt',function (txt){
-        var text = txt;
-        var split = [];
+        let text = txt;
+        let split = [];
         split =text.split('$$$');
-        var json =JSON.stringify(split)
-        //var data=[];
-        /*for (i=0;i<split.length;i+5){
-          //  var myObj={labels:split[i],special_labels:split[i+1],origina_labels:split[i+2],title:split[i+3],text:split[i+4]};
-            //data.push(myObj);
-            console.log(i);
-        }*/
-        console.log(json);
 
+
+        //var data=[];
+        for (i=0;i<split.length;i++){
+            console.log(i);
+            
+
+        }
+        //split.forEach(
+          //  element => asd(element)
+        //);
+
+        //console.log (split);
     });
+}
+function asd(element){
+
+    //document.getElementById('output').innerHTML+=element;
 }
