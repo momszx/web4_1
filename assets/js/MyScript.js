798f24dabe21244fb656265f3a79188a54d91ef5
Donkó Márk <donkomark@gmail.com>
2020-10-12 21:53:14 +0200

    Selecton működik megfelelően

--- assets/js/MyScript.js
+++ assets/js/MyScript.js
@@ -1,8 +1,10 @@
+let splitby$=[];
+let a=0;
 function readFile(){
     jQuery.get('assets/data.txt',function (txt){
         let text = txt;
         let split = []; // Ebben van a data.txt tömbje soronként értelmezve
-        let splitby$=[]; // Ebben van a soronként értelmezet adat feldarabolva $$$ onként
+         // Ebben van a soronként értelmezet adat feldarabolva $$$ onként
         split =text.split('\n'); // itt darabolom fel sorokra
         for (i=0;i<split.length;i++){
             splitby$.push(split[i].split('$$$')); // itt darabolom fel a sorokat $$$ onként
@@ -38,11 +40,71 @@ function readFile(){
                 splitby$[i][n]=splitByLabel;
             }
         }
-        console.log(splitby$);
         asd(splitby$)
+        addOption(a)
     });
 }
 function asd(splitby$){
     let rnd =Math.floor(Math.random(0,splitby$.length) * 100) + 1
     document.getElementById('cikk').innerHTML+=splitby$[rnd][4];
+}
+//Ez írja ki a valószíbűséget
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value/100;
+
+slider.oninput = function() {
+    output.innerHTML = this.value/100;
+}
+//Ez adja hozzá az elemeket a Selectsonhöz
+function addOption(a){
+    var x = document.getElementById("mySelect");
+
+    for (d=a;d<a+20;d++) {
+        var option = document.createElement("option");
+        if (d<2000){
+            option.text = splitby$[d][3];
+            x.add(option);
+        }
+        else {
+            option.text=splitby$[d-2000][3];
+            x.add(option);
+        }
+    }
+}
+function SelectChange() {
+    var x = document.getElementById("mySelect");
+    var i = x.selectedIndex;
+    if ((a+i)<2000){
+        document.getElementById("cikk").innerHTML = splitby$[a+i][4];
+    }
+    else {
+        document.getElementById("cikk").innerHTML = splitby$[(a+i)-2000][4];
+    }
+}
+function addPlus(){
+    a++;
+    removeSelect();
+    addOption(a)
+}
+function addMinus(){
+    a--;
+    removeSelect();
+    addOption(a)
+}
+function addPlus20Element(){
+    a+=20;
+    removeSelect();
+    addOption(a)
+}
+function addMinus20Element(){
+    a-=20;
+    removeSelect();
+    addOption(a)
+}
+function removeSelect() {
+    var x = document.getElementById("mySelect");
+    for (i=0;i<20;i++){
+        x.options.remove(i);
+    }
 }
